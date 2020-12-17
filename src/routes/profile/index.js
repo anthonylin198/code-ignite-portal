1ed7a517c0de21c405e4a38dd7c4b8c14cec6d70
anthonylin198 <anthony.lin.198@gmail.com>
2020-12-16 16:09:51 -0800

    edit columns

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Column, Row } from "simple-flexbox";
 
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <Row horizontal="space-between" breakpoints={{ 768: "column" }}>
+      <Column>
+        <h1>Profile Component</h1>
+        <h1>Summary Component</h1>
+        <h1>friends Component</h1>
+      </Column>
+      <Column breakpoints={{ 768: "column" }}>
+        <h1>Your Company Componentttttt</h1>
+        <h1>Progress Component</h1>
+      </Column>
+    </Row>
   );
 };
 
