b88637ba1a46c19a83b7c63eb88643597a95f680
anthonylin198 <anthony.lin.198@gmail.com>
2020-12-18 12:03:52 -0800

    revert changes

--- dbutils/dbConnect.js
+++ dbutils/dbConnect.js
@@ -7,17 +7,20 @@ async function dbConnect() {
     return;
   }
 
-  let mongoURI = "";
-  if (process.env.NODE_ENV === "development") {
-    mongoURI = process.env.REACT_APP_MONGO_URI_DEV;
-  } else {
-    mongoURI = process.env.REACT_APP_MONGO_URI_PROD;
-  }
+  // let mongoURI = "";
+  // if (process.env.NODE_ENV === "development") {
+  //   mongoURI = process.env.REACT_APP_MONGO_URI_DEV;
+  // } else {
+  //   mongoURI = process.env.REACT_APP_MONGO_URI_PROD;
+  // }
 
-  const db = await mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const db = await mongoose.connect(
+    "mongodb+srv://anthonylin198:anthonylin198@igniteprogrammingdb.ucsf3.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 
   connection.isConnected = db.connections[0].readyState;
   // console.log(connection.isConnected);
