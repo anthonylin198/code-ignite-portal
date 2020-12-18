f8656fe8a41dc453af71e2ace615c3b933a501b8
anthonylin198 <anthony.lin.198@gmail.com>
2020-12-18 15:18:20 -0800

    styling header

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -2,68 +2,131 @@ import React, { useContext } from "react";
 import { string } from "prop-types";
 import { useHistory } from "react-router-dom";
 import { Row } from "simple-flexbox";
-import { createUseStyles, useTheme } from "react-jss";
+// import { createUseStyles, useTheme } from "react-jss";
 import { SidebarContext } from "../../hooks/useSidebar";
 import SLUGS from "../../resources/slugs";
 import { IconBell, IconSearch } from "../../assets/icons";
 import DropdownComponent from "../../components/dropdown";
 
-const useStyles = createUseStyles((theme) => ({
-  avatar: {
-    height: 35,
-    width: 35,
-    minWidth: 35,
-    borderRadius: 50,
-    marginLeft: 14,
-    border: `1px solid "#DFE0EB"`,
-    "@media (max-width: 768px)": {
-      marginLeft: 14,
-    },
-  },
-  container: {
-    height: 40,
-  },
-  name: {
-    // ...theme.typography.itemTitle,
-    textAlign: "right",
-    "@media (max-width: 768px)": {
-      display: "none",
-    },
-  },
-  separator: {
-    borderLeft: `1px solid "#DFE0EB"`,
-    marginLeft: 32,
-    marginRight: 32,
-    height: 32,
-    width: 2,
-    "@media (max-width: 768px)": {
-      marginLeft: 14,
-      marginRight: 0,
-    },
-  },
-  title: {
-    // ...theme.typography.title,
-    "@media (max-width: 1080px)": {
-      marginLeft: 50,
-    },
-    "@media (max-width: 468px)": {
-      fontSize: 20,
-    },
-  },
-  iconStyles: {
-    cursor: "pointer",
-    marginLeft: 25,
-    "@media (max-width: 768px)": {
-      marginLeft: 12,
-    },
-  },
-}));
+import styled from "styled-components";
+
+const Container = styled(Row)`
+  height: 40px;
+`;
+
+const IconStyles = styled.div`
+  cursor: pointer;
+  margin-left: 25px;
+  @media (max-width: 768px) {
+    margin-left: 12px;
+  }
+`;
+
+const Separator = styled.div`
+  border-left: 1px solid #dfe0eb;
+  margin-left: 32px;
+  margin-right: 32px;
+  height: 32px;
+  width: 2px;
+  @media (max-width: 768px) {
+    margin-left: 14px;
+    margin-right: 0;
+  }
+`;
+
+const Title = styled.span`
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 30px;
+  letter-spacing: 0.3px;
+
+  @media (max-width: 1080px) {
+    margin-left: 50px;
+  }
+  @media (max-width: 468px) {
+    font-size: 20px;
+  }
+`;
+
+const Name = styled.span`
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.2px;
+  text-align: right;
+  @media (max-width: 768px) {
+    display: "none";
+  }
+`;
+
+const Avatar = styled.img`
+  height: 35px;
+  width: 35px;
+  min-width: 35px;
+  border-radius: 50px;
+  margin-left: 14px;
+  border: 1px solid #dfe0eb;
+  @media (max-width: 768px) {
+    margin-left: 14px;
+  }
+`;
+
+// const useStyles = createUseStyles((theme) => ({
+//   avatar: {
+//     height: 35,
+//     width: 35,
+//     minWidth: 35,
+//     borderRadius: 50,
+//     marginLeft: 14,
+//     border: `1px solid "#DFE0EB"`,
+//     "@media (max-width: 768px)": {
+//       marginLeft: 14,
+//     },
+//   },
+//   container: {
+//     height: 40,
+//   },
+//   name: {
+//     // ...theme.typography.itemTitle,
+//     textAlign: "right",
+//     "@media (max-width: 768px)": {
+//       display: "none",
+//     },
+//   },
+//   separator: {
+//     borderLeft: `1px solid "#DFE0EB"`,
+//     marginLeft: 32,
+//     marginRight: 32,
+//     height: 32,
+//     width: 2,
+//     "@media (max-width: 768px)": {
+//       marginLeft: 14,
+//       marginRight: 0,
+//     },
+//   },
+//   title: {
+//     // ...theme.typography.title,
+//     "@media (max-width: 1080px)": {
+//       marginLeft: 50,
+//     },
+//     "@media (max-width: 468px)": {
+//       fontSize: 20,
+//     },
+//   },
+//   iconStyles: {
+//     cursor: "pointer",
+//     marginLeft: 25,
+//     "@media (max-width: 768px)": {
+//       marginLeft: 12,
+//     },
+//   },
+// }));
 
 function HeaderComponent() {
   const { push } = useHistory();
   const { currentItem } = useContext(SidebarContext);
-  const theme = useTheme();
-  const classes = useStyles({ theme });
+  // const theme = useTheme();
+  // const classes = useStyles({ theme });
 
   let title;
 
@@ -118,17 +181,13 @@ function HeaderComponent() {
   }
 
   return (
-    <Row
-      className={classes.container}
-      vertical="center"
-      horizontal="space-between"
-    >
-      <span className={classes.title}>{title}</span>
+    <Container vertical="center" horizontal="space-between">
+      <Title>{title}</Title>
       <Row vertical="center">
-        <div className={classes.iconStyles}>
+        <IconStyles>
           <IconSearch />
-        </div>
-        <div className={classes.iconStyles}>
+        </IconStyles>
+        <IconStyles>
           <DropdownComponent
             label={<IconBell />}
             options={[
@@ -154,16 +213,16 @@ function HeaderComponent() {
               right: -14,
             }}
           />
-        </div>
-        <div className={classes.separator}></div>
+        </IconStyles>
+        <Separator></Separator>
         <DropdownComponent
           label={
             <>
-              <span className={classes.name}>Anthony Lin</span>
-              <img
+              <Name>Anthony Lin</Name>
+              <Avatar
                 src="https://avatars3.githubusercontent.com/u/21162888?s=460&v=4"
                 alt="avatar"
-                className={classes.avatar}
+                // className={classes.avatar}
               />
             </>
           }
@@ -183,7 +242,7 @@ function HeaderComponent() {
           }}
         />
       </Row>
-    </Row>
+    </Container>
   );
 }
 
